ce744ccbe2bde846c5a4f18ab2e1ce88df972e91
Konstantin <k.podyganov@mail.ru>
2020-03-20 13:43:02 +0300

    Moved files to folder

---
+++ task1/src/like.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Like extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      liked: false
+    };
+  }
+
+  render() {
+    const { liked } = this.state;
+    return React.createElement('div', { onClick: () => this.setState({ liked: !liked }) }, `${liked ? 'liked' : 'unliked'}`);
+  }
+}
+
+const domContainer = document.querySelector('#like');
+ReactDOM.render(React.createElement(Like), domContainer);
